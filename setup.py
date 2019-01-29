619cb5f7e2d784d8e1e763b54491bc1d7e9e4967
Manfred Diaz <takeitallsource@gmail.com>
2019-01-28 23:31:04 -0500

    Is it?

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='safe_iil',
+    version='',
+    packages=['', '_utils', 'learners', 'learners.parametrizations', 'learners.parametrizations.tf',
+              'learners.parametrizations.tf._layers', 'learners.parametrizations.tf._layers.autoencoders',
+              'learners.parametrizations.tf.uncertainty', 'teachers', 'algorithms'],
+    url='',
+    license='',
+    author='linuxpotter',
+    author_email='',
+    description=''
+)
